d3a168b03f4d7399a2500de0984392b72c920acb
Vanshika Dargan <vanshikadargan.vd@gmail.com>
2024-01-27 15:51:35 +0530

    Time: 9 ms (29.64%) | Memory: 42.4 MB (44.27%) - LeetSync

--- Leetcode-Solutions/576-out-of-boundary-paths/out-of-boundary-paths.java
+++ Leetcode-Solutions/576-out-of-boundary-paths/out-of-boundary-paths.java
@@ -2,29 +2,26 @@ class Solution {
     int dir[][]={{0,1},{0,-1},{1,0},{-1,0}};
     int mod=(int)1e9+7;
     public int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
-        Integer dp[][][]=new Integer[51][51][51];
-        return helper(m,n,maxMove,startRow,startColumn,0,dp);
+        int dp[][][]=new int[2][51][51];
+        for(int curr=1;curr<=maxMove;curr++){
+            for(int i=0;i<m;i++){
+                for(int j=0;j<n;j++){
+                    int ans=0;
+                    for(int d[]:dir){
+                    
+              int nextRow=d[0]+i;
+              int nextColumn=d[1]+j;
+              if((nextRow==-1 ||  nextRow==m ||   nextColumn==-1 ||  nextColumn==n))
+              ans++;
+              else
+              ans=(ans+dp[(curr-1)&1][nextRow][nextColumn])%mod;
+    }
+    dp[curr&1][i][j]=ans;
+                }
+            }
+        }
+        return dp[maxMove&1][startRow][startColumn];
+        
     }
 
-    int helper(int m,int n,int maxMove,int startRow,int startColumn,int curr,Integer dp[][][]){
-    
-    if((startRow==-1 || startRow==m || startColumn==-1 || startColumn==n) && curr<=maxMove){
-        
-        return 1;
-    }
-    if(curr>maxMove){
-        return 0;
-    }
-    if(dp[startRow][startColumn][curr]!=null){
-        return dp[startRow][startColumn][curr];
-    }
-    int ans=0;
-    for(int d[]:dir){
-        int nextRow=d[0]+startRow;
-        int nextColumn=d[1]+startColumn;
-        ans=(ans+helper(m,n,maxMove,nextRow,nextColumn,curr+1,dp))%mod;
-    }
-    dp[startRow][startColumn][curr]=ans;
-    return ans;
-    }
 }
