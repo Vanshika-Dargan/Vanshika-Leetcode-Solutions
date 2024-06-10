e06b8e72cbc6f21cd3e92631898bb937aab5d4ef
Vanshika Dargan <vanshikadargan.vd@gmail.com>
2024-06-10 22:46:02 +0530

    Time: 2 ms (88.93%) | Memory: 40.9 MB (90.77%) - LeetSync

--- Leetcode-Solutions/1137-height-checker/height-checker.java
+++ Leetcode-Solutions/1137-height-checker/height-checker.java
@@ -5,8 +5,6 @@ class Solution {
         Arrays.sort(heights);
         int count=0;
         for(int i=0;i<heights.length;i++){
-            System.out.println(heights[i]);
-            System.out.println(original[i]);
             if(original[i]!=heights[i]) 
             count++;
         }
