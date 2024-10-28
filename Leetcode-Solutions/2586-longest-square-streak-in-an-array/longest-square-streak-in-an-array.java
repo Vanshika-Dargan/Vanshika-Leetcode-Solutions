28de2ea9786a0d7b29472a805b58aa19f67d9cf3
dragonnnn <vanshikadargan.vd@gmail.com>
2024-10-28 23:21:29 +0530

    Time: 68 ms (12.50%) | Memory: 61.2 MB (36.11%) - LeetSync

---
+++ Leetcode-Solutions/2586-longest-square-streak-in-an-array/longest-square-streak-in-an-array.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int longestSquareStreak(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        Set<Integer> seen = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.putIfAbsent(nums[i], 1);
+        }
+        int max = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (seen.contains(nums[i])) continue;
+            long searchDouble = (long) nums[i] * nums[i];
+            
+            int searchHalf = -1;
+            int sqrt = (int) Math.sqrt(nums[i]);
+            if (sqrt * sqrt == nums[i]) {
+                searchHalf = sqrt;
+            }
+            if (map.containsKey(searchHalf)) {
+                map.put(nums[i], map.get(searchHalf));
+            }
+            if (searchDouble < 100000 && map.containsKey((int) searchDouble)) {
+                System.out.println(nums[i]);
+                System.out.println(searchDouble);
+                map.put(nums[i], map.get(nums[i]) + map.get((int) searchDouble));
+            }
+            seen.add(nums[i]);
+            max = Math.max(max, map.get(nums[i]));
+        }
+        return max == 1 ? -1 : max;
+    }
+}
