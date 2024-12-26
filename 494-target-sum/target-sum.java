6f926580313a6f59e8acefa3f0e6d80fe71036c3
dragonnnn <vanshikadargan.vd@gmail.com>
2024-12-26 23:24:35 +0530

    Time: 11 ms (58.68%) | Memory: 44.8 MB (31.17%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,22 +1,32 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        
-        int res[]=new int[1];
-        helper(0,nums,target,0,res);
-        return res[0];
-    }
-
-    void helper(int i,int[] nums,int target,int curr,int[] res){
-        if(i==nums.length && curr!=target){
-            res[0]+=0;
-            return;
-        }
-        else if(i==nums.length && curr==target) {
-            res[0]+=1;
-            return;
+        int sum = 0;
+        for (int n : nums) {
+            sum += n;
         }
 
-        helper(i+1,nums,target,curr+nums[i],res);
-        helper(i+1,nums,target,curr-nums[i],res);
+        if (Math.abs(target) > sum) {
+            return 0;
+        }
+
+        int totalRange = (2 * sum) + 1;
+        int[][] dp = new int[nums.length][totalRange];
+
+        dp[nums.length - 1][sum + nums[nums.length - 1]]++;
+        dp[nums.length - 1][sum - nums[nums.length - 1]]++;
+
+
+        for (int i = nums.length - 2; i >= 0; i--) {
+            for (int curr = 0; curr < totalRange; curr++) {
+                if (curr + nums[i] < totalRange) {
+                    dp[i][curr] += dp[i + 1][curr + nums[i]];
+                }
+                if (curr - nums[i] >= 0) {
+                    dp[i][curr] += dp[i + 1][curr - nums[i]];
+                }
+            }
+        }
+
+        return dp[0][sum + target];
     }
 }
