4fdaedb3e7fb38e70630048f7b43b330f00e42c5
dragonnnn <vanshikadargan.vd@gmail.com>
2024-12-29 16:08:17 +0530

    Time: 2641 ms (5.13%) | Memory: 53.1 MB (52.01%) - LeetSync

--- 1744-number-of-ways-to-form-a-target-string-given-a-dictionary/number-of-ways-to-form-a-target-string-given-a-dictionary.java
+++ 1744-number-of-ways-to-form-a-target-string-given-a-dictionary/number-of-ways-to-form-a-target-string-given-a-dictionary.java
@@ -1,38 +1,27 @@
-import java.util.Arrays;
-
 class Solution {
+     int mod = 1_000_000_007;
     public int numWays(String[] words, String target) {
         int n = words.length;
         int m = words[0].length();
-        int mod = 1_000_000_007;
-
         int[][] freq = new int[26][m];
-
-        // Build frequency matrix
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 char ch = words[i].charAt(j);
                 freq[ch - 'a'][j] += 1;
             }
         }
-
-        // Initialize DP array
         int[][] dp = new int[target.length()][m + 1];
         for (int[] row : dp) {
             Arrays.fill(row, -1);
         }
-
-        // Call helper function
-        return helper(freq, target, 0, m, -1, mod, dp);
+        return helper(freq, target, 0, m, -1, dp);
     }
 
-    int helper(int[][] freq, String target, int i, int m, int last, int mod, int[][] dp) {
-        // Base case: all characters in the target are processed
+    int helper(int[][] freq, String target, int i, int m, int last, int[][] dp) {
         if (i == target.length()) {
             return 1;
         }
 
-        // Memoization check
         if (dp[i][last + 1] != -1) {
             return dp[i][last + 1];
         }
@@ -41,15 +30,11 @@ class Solution {
         int c = ch - 'a';
         int count = 0;
 
-        // Try all positions from last + 1 to m for the current character
         for (int j = last + 1; j < m; j++) {
             if (freq[c][j] != 0) {
-                // Multiply frequency and recursively calculate for the next character
-                count = (count + (int)((long)freq[c][j] * helper(freq, target, i + 1, m, j, mod, dp) % mod)) % mod;
+                count = (count + (int)((long)freq[c][j] * helper(freq, target, i + 1, m, j, dp) % mod)) % mod;
             }
         }
-
-        // Store result in DP array
         dp[i][last + 1] = count;
         return count;
     }
