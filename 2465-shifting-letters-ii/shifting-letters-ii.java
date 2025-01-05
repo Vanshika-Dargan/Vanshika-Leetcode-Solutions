d951e87f4f206266d048c53cd168e4fd53296593
dragonnnn <vanshikadargan.vd@gmail.com>
2025-01-05 12:58:01 +0530

    Time: 7 ms (74.53%) | Memory: 74.9 MB (41.04%) - LeetSync

--- 2465-shifting-letters-ii/shifting-letters-ii.java
+++ 2465-shifting-letters-ii/shifting-letters-ii.java
@@ -1,18 +1,25 @@
 class Solution {
     public String shiftingLetters(String s, int[][] shifts) {
         int m=s.length();
-        int pre[]=new int[m];
+        int pre[]=new int[m+1];
         for(int[] shift: shifts){
             int start=shift[0];
             int end=shift[1];
             int dir=shift[2];
-            for(int i=start;i<=end;i++){
-              pre[i] += (dir == 0) ? -1 : 1;
+            if(dir==1){
+            pre[start]+=1;
+            pre[end+1]-=1;
+            }
+            else{
+            pre[start]-=1;
+            pre[end+1]+=1;
             }
         }
+        int noOfShifts=0;
         StringBuilder sb = new StringBuilder(s);
-         for (int i = 0; i < m; i++) {
-            int shift = pre[i] % 26;
+         for (int i = 0; i < m; i++) {    
+             noOfShifts=noOfShifts + pre[i];
+            int shift = noOfShifts % 26;
             if (shift < 0) shift += 26; 
             char newChar = (char) ((s.charAt(i) - 'a' + shift) % 26 + 'a');
             sb.setCharAt(i, newChar);
