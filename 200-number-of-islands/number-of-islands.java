5a9413a8e8eaa1a703adabe4b17b8498c6abe41c
vanshika-dargan <vanshikadargan.vd@gmail.com>
2025-12-29 15:52:56 +0530

    Time: 3 ms (86.68%) | Memory: 52.4 MB (42.67%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,31 +1,34 @@
 class Solution {
+    static int dir[][] = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
+
     public boolean isValid(int i, int j, int m, int n) {
         if (i < 0 || j < 0 || i >= m || j >= n)
             return false;
         return true;
     }
 
+    public void dfs(int i, int j, char[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        grid[i][j] = '2';
+        for (int d[] : dir) {
+            int new_i = d[0] + i;
+            int new_j = d[1] + j;
+            if (isValid(new_i, new_j, m, n) && grid[new_i][new_j] == '1') {
+                dfs(new_i, new_j, grid);
+            }
+        }
+    }
+
     public int numIslands(char[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        LinkedList<int[]> q = new LinkedList<>();
-        int dir[][] = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
         int island = 0;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == '1') {
                     island++;
-                    grid[i][j] = '2';
-                    q.offer(new int[] { i, j });
-                    while (!q.isEmpty()) {
-                        int node[] = q.poll();
-                        for (int d[] : dir) {
-                            if (isValid(node[0] + d[0], node[1] + d[1], m, n) && grid[node[0] + d[0]][node[1] + d[1]] == '1'){
-                                grid[node[0] + d[0]][node[1] + d[1]]='2';
-                                q.offer(new int[] { node[0] + d[0], node[1] + d[1] });
-                            }
-                        }
-                    }
+                    dfs(i, j, grid);
                 }
             }
         }
