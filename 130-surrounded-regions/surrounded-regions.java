57950167ba3517aebc198573c7d86146f28c349a
vanshika-dargan <vanshikadargan.vd@gmail.com>
2026-01-04 18:15:43 +0530

    Time: 2 ms (82.14%) | Memory: 47.8 MB (55.37%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -1,31 +1,33 @@
 class Solution {
-    public boolean isValid(int i, int j, int m, int n){
-        if(i<0 || j<0 || i>=m ||j>=n) return false;
+    static int dir[][] = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
+
+    public boolean isValid(int i, int j, int m, int n) {
+        if (i < 0 || j < 0 || i >= m || j >= n)
+            return false;
         return true;
     }
+
+    public void dfs(int i, int j, int m, int n, boolean[][] v,char[][] board) {
+        v[i][j] = true;
+        for (int d[] : dir) {
+            int new_i = d[0] + i;
+            int new_j = d[1] + j;
+            if (isValid(new_i, new_j, m, n) && board[new_i][new_j] == 'O' && !v[new_i][new_j]) {
+                dfs(new_i, new_j, m, n, v,board);
+            }
+        }
+    }
+
     public void solve(char[][] board) {
         int m = board.length;
         int n = board[0].length;
         boolean v[][] = new boolean[m][n];
-        LinkedList<int[]> q = new LinkedList<>();
-        int dir[][] = {{0,1},{1,0},{0,-1},{-1,0}};
+       
         
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(board[i][j]=='O' && (i==0 || j==0 || i==m-1 || j==n-1)){
-                    q.offer(new int[]{i,j});
-                    v[i][j]=true;
-                }
-            }
-        }
-        while(!q.isEmpty()){
-            int node[]=q.poll();
-            for(int d[]:dir){
-                int new_i = d[0]+node[0];
-                int new_j = d[1]+node[1];
-                if(isValid(new_i,new_j,m,n) && board[new_i][new_j]=='O' && !v[new_i][new_j]){
-                    q.offer(new int[]{new_i,new_j});
-                    v[new_i][new_j]=true;
+                    dfs(i,j,m,n,v,board);
                 }
             }
         }
