af572f1479d5fa37415a950bddcf834ad810bb95
vanshika-dargan <vanshikadargan.vd@gmail.com>
2026-02-26 19:18:44 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (11.45%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,18 +1,28 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int dp[][]=new int[m][n];
-        for(int row[]:dp){
-            Arrays.fill(row,-1);
+        int dp[][] = new int[m + 1][n + 1];
+        dp[m - 1][n - 1] = 1;
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                if(i==m-1 && j==n-1) continue;
+                int right = dp[i + 1][j];
+                int down = dp[i][j + 1];
+                dp[i][j] = right + down;
+            }
         }
-        return fn(0,0,m,n,dp);
+        return dp[0][0];
+        // return fn(0,0,m,n,dp);
     }
 
-    int fn(int i, int j,int m, int n,int[][] dp){
-        if(i==m-1 && j==n-1) return 1;
-        if(i>=m || j>=n) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        int right = fn(i+1,j,m,n,dp);
-        int down = fn(i,j+1,m,n,dp);
-        return dp[i][j]=right + down;
+    int fn(int i, int j, int m, int n, int[][] dp) {
+        if (i == m - 1 && j == n - 1)
+            return 1;
+        if (i >= m || j >= n)
+            return 0;
+        if (dp[i][j] != -1)
+            return dp[i][j];
+        int right = fn(i + 1, j, m, n, dp);
+        int down = fn(i, j + 1, m, n, dp);
+        return dp[i][j] = right + down;
     }
 }
