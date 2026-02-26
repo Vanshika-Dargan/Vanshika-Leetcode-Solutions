0201c1d340ea87d4b49682a30818cd7a07e33e75
vanshika-dargan <vanshikadargan.vd@gmail.com>
2026-02-27 01:25:37 +0530

    Time: 1 ms (17.66%) | Memory: 43.5 MB (64.96%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -2,22 +2,37 @@ class Solution {
     public int uniquePathsWithObstacles(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        int dp[][] = new int[m][n];
-        for(int row[]:dp){
-            Arrays.fill(row,-1);
+        int dp[][] = new int[m+1][n+1];
+        if (grid[m - 1][n - 1] == 1)
+            return 0;
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                if (i == m - 1 && j == n - 1){
+                    dp[i][j] = 1;
+                    continue;
+                    }
+                if (grid[i][j] == 1){
+                    dp[i][j] = 0;
+                    continue;
+                }
+                int right = dp[i + 1][j];
+                int down = dp[i][j + 1];
+                dp[i][j] = right + down;
+            }
         }
-        if(grid[m-1][n-1]==1) return 0;
-        return fn(0,0,m,n,grid,dp);
+        return dp[0][0];
     }
-        int fn(int i, int j, int m, int n, int[][] grid,int[][] dp) {
+
+    int fn(int i, int j, int m, int n, int[][] grid, int[][] dp) {
         if (i == m - 1 && j == n - 1)
             return 1;
-        if (i >= m || j >= n || grid[i][j]==1)
+        if (i >= m || j >= n || grid[i][j] == 1)
             return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        int right = fn(i + 1, j, m, n, grid,dp);
-        int down = fn(i, j + 1, m, n, grid,dp);
-        return dp[i][j]=right + down;
+        if (dp[i][j] != -1)
+            return dp[i][j];
+        int right = fn(i + 1, j, m, n, grid, dp);
+        int down = fn(i, j + 1, m, n, grid, dp);
+        return dp[i][j] = right + down;
     }
 }
 
@@ -38,6 +53,5 @@ class Solution {
 //         return dp[0];
 //         // return fn(0,0,m,n,dp);
 //     }
-
 
 // }
