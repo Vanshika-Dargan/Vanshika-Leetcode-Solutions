28d0101e29288b798ec356e8a9018d1be8a4d802
vanshika-dargan <vanshikadargan.vd@gmail.com>
2026-02-28 15:38:23 +0530

    Time: 5 ms (25.68%) | Memory: 50.2 MB (85.26%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -2,28 +2,30 @@ class Solution {
     public int minPathSum(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        int dp[][] = new int[m][n];
+        int dp[]= new int[n];
         // for(int row[]:dp)
         // Arrays.fill(row,-1);
         // return fn(grid,m-1,n-1,dp);
-        dp[0][0] = grid[0][0];
+        dp[0] = grid[0][0];
         for (int i = 0; i < m; i++) {
+            int temp[]=new int[n];
             for (int j = 0; j < n; j++) {
                 int up = Integer.MAX_VALUE;
                 int left = Integer.MAX_VALUE;
                 if(i>0)
-                up = dp[i - 1][j];
+                up = dp[j];
                 if(j>0)
-                left = dp[i][j - 1];
+                left = temp[j - 1];
 
-                dp[i][j] = grid[i][j];
+                temp[j] = grid[i][j];
                 int min = Math.min(up, left);
                 if(min!=Integer.MAX_VALUE){
-                    dp[i][j]+=min;
+                    temp[j]+=min;
                 }
             }
+            dp = temp;
         }
-        return dp[m-1][n-1];
+        return dp[n-1];
     }
 
     int fn(int[][] grid, int i, int j, int dp[][]) {
