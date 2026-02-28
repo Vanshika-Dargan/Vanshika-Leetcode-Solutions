78c5c9677666745025606cce18034f6a6dbc9be8
vanshika-dargan <vanshikadargan.vd@gmail.com>
2026-03-01 02:17:13 +0530

    Time: 8 ms (30.81%) | Memory: 47.4 MB (46.67%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.java
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -3,13 +3,28 @@ class Solution {
         int min = Integer.MAX_VALUE;
         int n = matrix.length;
         int dp[][] = new int[n][n];
-        for (int row[] : dp) {
-            Arrays.fill(row, Integer.MAX_VALUE);
+        for (int j = 0; j < n; j++) {
+            dp[0][j] = matrix[0][j];
         }
-        for (int i = 0; i < n; i++) {
-            min = Math.min(min, fn(matrix, 0, i,dp));
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int a = Integer.MAX_VALUE;
+                int b = Integer.MAX_VALUE;
+                int c = Integer.MAX_VALUE;
+                if (j - 1 >= 0)
+                    a = dp[i - 1][j - 1];
+                b = dp[i - 1][j];
+                if (j + 1 < n)
+                    c = dp[i - 1][j + 1];
+                 min = Math.min(a, Math.min(b, c));
+                dp[i][j] = matrix[i][j] + min;
+            }
         }
-        return min;
+        int ans = Integer.MAX_VALUE;
+        for(int i=0;i<n;i++){
+            ans = Math.min(ans,dp[n-1][i]);
+        }
+        return ans;
     }
 
     int fn(int[][] matrix, int i, int j, int dp[][]) {
@@ -24,8 +39,8 @@ class Solution {
         int c = fn(matrix, i + 1, j + 1, dp);
         int min = Math.min(a, Math.min(b, c));
         if (min == Integer.MAX_VALUE)
-        dp[i][j] = Integer.MAX_VALUE;
-        dp[i][j]=matrix[i][j] + min;
+            dp[i][j] = Integer.MAX_VALUE;
+        dp[i][j] = matrix[i][j] + min;
         return dp[i][j];
     }
 }
