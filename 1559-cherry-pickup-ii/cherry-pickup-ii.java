ea4388fc007a5364f0eea5e82e2b6efac07a3bc3
vanshika-dargan <vanshikadargan.vd@gmail.com>
2026-03-06 09:20:26 +0530

    Time: 40 ms (12.67%) | Memory: 46.3 MB (88.69%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.java
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.java
@@ -3,12 +3,40 @@ class Solution {
         int m = grid[0].length;
         int n = grid.length;
         int dp[][][] = new int[n][m][m];
-        for (int box[][] : dp) {
-            for (int row[] : box) {
-                Arrays.fill(row, -1);
+
+        for (int j1 = 0; j1 < m; j1++) {
+            for (int j2 = 0; j2 < m; j2++) {
+                if (j1 == j2)
+                    dp[n - 1][j1][j2] = grid[n - 1][j1];
+                else
+                    dp[n - 1][j1][j2] = grid[n - 1][j1] + grid[n - 1][j2];
             }
         }
-        return fn(grid, 0, 0, grid[0].length - 1,dp);
+
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j1 = m - 1; j1 >= 0; j1--) {
+                for (int j2 = m - 1; j2 >= 0; j2--) {
+                    int max = Integer.MIN_VALUE;
+                    for (int d1 = -1; d1 <= 1; d1++) {
+                        int temp = Integer.MIN_VALUE;
+                        for (int d2 = -1; d2 <= 1; d2++) {
+                            if (j1 + d1 < 0 || j1 + d1 >= m || j2 + d2 < 0 || j2 + d2 >= m)
+                                continue;
+                            temp = dp[i + 1][d1 + j1][d2 + j2];
+                            if (temp == Integer.MIN_VALUE)
+                                continue;
+                            temp += grid[i][j1];
+                            if (j1 != j2) {
+                                temp += grid[i][j2];
+                            }
+                            max = Math.max(max, temp);
+                        }
+                    }
+                    dp[i][j1][j2] = max;
+                }
+            }
+        }
+        return dp[0][0][m - 1];
     }
 
     int fn(int[][] grid, int i, int j1, int j2, int dp[][][]) {
@@ -27,7 +55,7 @@ class Solution {
             int temp = Integer.MIN_VALUE;
             for (int d2 = -1; d2 <= 1; d2++) {
 
-                temp = fn(grid, i + 1, d1 + j1, d2 + j2,dp);
+                temp = fn(grid, i + 1, d1 + j1, d2 + j2, dp);
                 if (temp == Integer.MIN_VALUE)
                     continue;
                 temp += grid[i][j1];
