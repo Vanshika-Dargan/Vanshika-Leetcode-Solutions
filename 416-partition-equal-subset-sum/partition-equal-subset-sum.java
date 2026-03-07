f65d5445b2d3f3e3898982ae30b81e7c1653cf79
vanshika-dargan <vanshikadargan.vd@gmail.com>
2026-03-07 16:14:54 +0530

    Time: 50 ms (48.72%) | Memory: 47.7 MB (56.87%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -5,25 +5,41 @@ class Solution {
         for (int i = 0; i < n; i++) {
             sum += nums[i];
         }
-        if(sum%2!=0) return false;
-        int target = sum/2;
-        int dp[][] = new int[n][target+1];
-            for (int row[] : dp) {
-                Arrays.fill(row, -1);
+        if (sum % 2 != 0)
+            return false;
+        int target = sum / 2;
+        boolean dp[][] = new boolean[n][target + 1];
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = true;
+        }
+        if (nums[0] <= target)
+            dp[0][nums[0]] = true;
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j <= target; j++) {
+                boolean take = false;
+                boolean not_take = dp[i - 1][j];
+                if (j - nums[i] >= 0)
+                    take = dp[i - 1][j - nums[i]];
+
+                dp[i][j] = take || not_take;
             }
-        return fn(nums, n-1,target, dp) == 1;
+        }
+        return dp[n - 1][target];
     }
 
     int fn(int[] nums, int i, int target, int dp[][]) {
-        if(target==0) return 1;
-        if(i<0) return 0;
+        if (target == 0)
+            return 1;
+        if (i < 0)
+            return 0;
         if (dp[i][target] != -1)
             return dp[i][target];
-         int take = 0;
-         int not_take = fn(nums,i-1,target,dp);
-         if(target-nums[i]>=0) 
-         take = fn(nums,i-1,target-nums[i],dp);
-         dp[i][target] = take | not_take;
-         return dp[i][target];
+        int take = 0;
+        int not_take = fn(nums, i - 1, target, dp);
+        if (target - nums[i] >= 0)
+            take = fn(nums, i - 1, target - nums[i], dp);
+        dp[i][target] = take | not_take;
+        return dp[i][target];
     }
 }
