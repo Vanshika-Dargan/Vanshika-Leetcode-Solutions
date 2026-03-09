f3642590a9b4e260069722ff839cc90c609bdd34
vanshika-dargan <vanshikadargan.vd@gmail.com>
2026-03-10 02:15:32 +0530

    Time: 14 ms (89.62%) | Memory: 46.2 MB (91.69%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,25 +1,38 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
-        int dp[] = new int[amount+1];
-        
-            Arrays.fill(dp,-1);
+        int dp[] = new int[amount + 1];
 
-        int ans = fn(coins,amount,dp);
+        dp[0] = 0;
+        for (int j = 1; j <= amount; j++) {
+            int min = Integer.MAX_VALUE;
+            for (int i = 0; i < coins.length; i++) {
+                int temp = Integer.MAX_VALUE;
+                if (j >= coins[i])
+                    temp = dp[j - coins[i]];
+                if (temp != Integer.MAX_VALUE)
+                    min = Math.min(temp + 1, min);
+                dp[j] = min;
+            }
+        }
+        int ans = dp[amount];
 
-        return ans==Integer.MAX_VALUE?-1:ans;
+        return ans == Integer.MAX_VALUE ? -1 : ans;
     }
 
-    int fn(int[] coins,int amount,int dp[]){
-        if(amount==0) return 0;
-        if(amount<0) return Integer.MAX_VALUE;
+    int fn(int[] coins, int amount, int dp[]) {
+        if (amount == 0)
+            return 0;
+        if (amount < 0)
+            return Integer.MAX_VALUE;
         int min = Integer.MAX_VALUE;
-         if(dp[amount]!=-1) return dp[amount];
-        for(int i=0;i<coins.length;i++){
-            
-            int temp = fn(coins,amount-coins[i],dp);
-            if(temp!=Integer.MAX_VALUE)
-            min = Math.min(temp+1,min);
+        if (dp[amount] != -1)
+            return dp[amount];
+        for (int i = 0; i < coins.length; i++) {
+
+            int temp = fn(coins, amount - coins[i], dp);
+            if (temp != Integer.MAX_VALUE)
+                min = Math.min(temp + 1, min);
             dp[amount] = min;
         }
         return min;
