bc00e03e5319d1da7c170cf0c69107d9caf88af6
vanshika-dargan <vanshikadargan.vd@gmail.com>
2026-03-11 02:16:03 +0530

    Time: 7 ms (64.97%) | Memory: 46.4 MB (30.66%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,32 +1,32 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
+        int n = nums.length;
         int sum = 0;
-        for (int n : nums) {
-            sum += n;
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
         }
+        int dp[][] = new int[n][2*sum+1];
+        for (int row[] : dp) {
+            Arrays.fill(row, -1);
+        }
+        return fn(nums, target, n - 1, dp,sum);
+    }
 
-        if (Math.abs(target) > sum) {
+    int fn(int[] nums, int target, int i, int dp[][],int offset) {
+        if (i == 0) {
+            if (nums[0] == 0 && target == 0) return 2;
+            if (target - nums[i] == 0 || target + nums[i] == 0)
+                return 1;
             return 0;
         }
-
-        int totalRange = (2 * sum) + 1;
-        int[][] dp = new int[nums.length][totalRange];
-
-        dp[nums.length - 1][sum + nums[nums.length - 1]]++;
-        dp[nums.length - 1][sum - nums[nums.length - 1]]++;
-
-
-        for (int i = nums.length - 2; i >= 0; i--) {
-            for (int curr = 0; curr < totalRange; curr++) {
-                if (curr + nums[i] < totalRange) {
-                    dp[i][curr] += dp[i + 1][curr + nums[i]];
-                }
-                if (curr - nums[i] >= 0) {
-                    dp[i][curr] += dp[i + 1][curr - nums[i]];
-                }
-            }
-        }
-
-        return dp[0][sum + target];
+        if (target > offset || target < -offset) return 0;
+        if (dp[i][target+offset] != -1)
+            return dp[i][target+offset];
+        int plus = 0;
+        int minus = 0;
+        plus = fn(nums, target - nums[i], i - 1, dp,offset);
+        minus = fn(nums, target + nums[i], i - 1, dp,offset);
+        dp[i][target+offset] = plus + minus;
+        return dp[i][target+offset];
     }
 }
