dd4ffb8d4dfeab88e6e20903509b4ccdebf86575
vanshika-dargan <vanshikadargan.vd@gmail.com>
2026-03-11 19:41:07 +0530

    Time: 11 ms (62.80%) | Memory: 57.2 MB (32.92%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,12 +1,26 @@
 class Solution {
-    public int change(int amount, int[] coins) {
+    public int change(int A, int[] coins) {
         int n = coins.length;
-        int dp[][] = new int[n][amount + 1];
-        for (int row[] : dp) {
-            Arrays.fill(row, -1);
-        }
+        int dp[][] = new int[n][A + 1];
         
-        return fn(coins, n - 1, amount, dp);
+        for(int amount=0;amount<=A;amount++){
+        if (amount % coins[0] == 0) {
+            dp[0][amount] = 1;
+        } else
+            dp[0][amount] = 0;
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int amount = 0; amount <= A; amount++) {
+                int pick = 0;
+                int not_pick = dp[i - 1][amount];
+                if (amount >= coins[i])
+                    pick = dp[i][amount - coins[i]];
+                dp[i][amount] = pick + not_pick;
+            }
+        }
+
+        return dp[n - 1][A];
     }
 
     int fn(int[] coins, int i, int amount, int dp[][]) {
@@ -18,9 +32,9 @@ class Solution {
         if (dp[i][amount] != -1)
             return dp[i][amount];
         int pick = 0;
-        int not_pick = fn(coins, i - 1, amount,dp);
+        int not_pick = fn(coins, i - 1, amount, dp);
         if (amount >= coins[i])
-            pick = fn(coins, i, amount - coins[i],dp);
+            pick = fn(coins, i, amount - coins[i], dp);
         dp[i][amount] = pick + not_pick;
         return dp[i][amount];
     }
