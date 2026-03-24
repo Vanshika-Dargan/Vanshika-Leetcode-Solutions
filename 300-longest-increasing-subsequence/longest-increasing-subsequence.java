ae285fe5c49962afcf5f0326897edf619ec8b2a6
vanshika-dargan <vanshikadargan.vd@gmail.com>
2026-03-24 14:27:16 +0530

    Time: 151 ms (16.89%) | Memory: 237.5 MB (5.86%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -2,10 +2,19 @@ class Solution {
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
         int dp[][] = new int[n+1][n+1];
-        for(int row[]:dp){
-            Arrays.fill(row,-1);
+        
+        for(int i=n-1;i>=0;i--){
+            for(int prev=0;prev<=n;prev++){
+                int not_take = dp[i+1][prev];
+        int take = 0;
+        if(prev==0 || nums[i]>nums[prev-1]){
+            take = 1 + dp[i+1][i+1];
         }
-        return fn(nums,0,-1,dp);
+
+         dp[i][prev] = Math.max(take,not_take);
+            }
+        }
+        return dp[0][0];
     }
 
     int fn(int[] nums,int i,int prev,int[][] dp){
@@ -13,14 +22,14 @@ class Solution {
         if(i==nums.length){
             return 0;
         }
-        if(dp[i][prev+1]!=-1) return dp[i][prev+1];
+        if(dp[i][prev]!=-1) return dp[i][prev];
         int not_take = fn(nums,i+1,prev,dp);
         int take = 0;
-        if(prev==-1 || nums[i]>nums[prev]){
-            take = 1 + fn(nums,i+1,i,dp);
+        if(prev==0 || nums[i]>nums[prev-1]){
+            take = 1 + fn(nums,i+1,i+1,dp);
         }
 
-         dp[i][prev+1] = Math.max(take,not_take);
-         return dp[i][prev+1];
+         dp[i][prev] = Math.max(take,not_take);
+         return dp[i][prev];
     }
 }
